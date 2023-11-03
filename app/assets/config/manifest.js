d2b10227c36b452e9cf44d478527dc3b09dffd48
infernal <nikita.dolgopolov@gmail.com>
2023-11-03 16:18:34 +0300

    add jsTree

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_tree ../stylesheets/plugins .css
 //= link_directory ../javascripts .js
 //= link_tree ../javascripts/resources .js
+//= link_tree ../javascripts/plugins .js
