215463a862182eff623d9313dec7f80a79ca15ef
infernal <nikita.dolgopolov@gmail.com>
2023-11-07 23:52:02 +0300

    helper improvements

--- app/helpers/web/lte_helper.rb
+++ app/helpers/web/lte_helper.rb
@@ -5,9 +5,9 @@ module Web::LteHelper
     options[:class] ||= 'btn-default'
 
     if options[:method].present?
-      link_to(options[:text], options[:path], method: options[:method], class: "btn #{options[:class]}").html_safe
+      link_to(options[:text], options[:path], method: options[:method], class: "btn-sm #{options[:class]}").html_safe
     else
-      link_to(options[:text], options[:path], class: "btn #{options[:class]}").html_safe
+      link_to(options[:text], options[:path], class: "btn-sm #{options[:class]}").html_safe
     end
   end
 
@@ -51,24 +51,29 @@ module Web::LteHelper
   ###--Кнопки--###
   def draw_edit_button(options)
     disabled = options[:disabled].present? ? ' disabled' : ''
-    link_to(icon('fas', 'edit', 'Редактировать'), options[:path], class: "btn btn-primary#{disabled}")
+    float = options[:float] == 'right' ? ' float-right' : ' float-left'
+    link_to(icon('fas', 'edit', 'Редактировать'), options[:path],
+            class: "btn-sm ml-1 mr-1 btn-primary#{disabled}#{float}")
   end
 
   def draw_back_button(options)
     disabled = options[:disabled].present? ? ' disabled' : ''
-    link_to(icon('fas', 'arrow-left', 'Назад'), options[:path], class: "btn btn-secondary#{disabled}")
+    float = options[:float] == 'right' ? ' float-right' : ' float-left'
+    link_to(icon('fas', 'arrow-left', 'Назад'), options[:path],
+            class: "btn-sm ml-1 mr-1 btn-secondary#{disabled}#{float}")
   end
 
   def draw_delete_button(options)
     disabled = options[:disabled].present? ? ' disabled' : ''
     options[:confirm_text] ||= 'Вы уверены?'
-    link_to(icon('fas', 'trash-alt', 'Удалить'), options[:path], method: :delete,
-                                                                 data: { confirm: options[:confirm_text] },
-                                                                 class: "btn btn-danger float-right#{disabled}")
+    link_to(icon('fas', 'trash-alt', 'Удалить'),
+            options[:path], method: :delete,
+                            data: { confirm: options[:confirm_text] },
+                            class: "btn-sm btn-danger float-right#{disabled}")
   end
 
   def draw_save_button(options = {})
-    # для кнопки за пределами формы необходимы параметры type="button", onclick="submit()"
+    # для кнопки за пределами формы необходимы параметры type="button", onclick="submit()", form="form_name"
     options[:type] ||= 'submit'
     options[:name] ||= 'button'
     form = options[:form].present? ? %( form="#{options[:form]}") : ''
@@ -77,8 +82,12 @@ module Web::LteHelper
     onclick = options[:onclick].present? ? %( onclick="#{options[:onclick]}" ) : ''
 
     out = %(
-            <button type="#{options[:type]}" class="btn btn-success float-right"#{form}#{id} name="#{options[:name]}"
-            #{onclick}#{disabled}><i class="fas fa-save"></i> Сохранить</button>
+      <button
+        type="#{options[:type]}"
+        class="btn-sm border-0 btn-success float-right"#{form}#{id}
+        name="#{options[:name]}"#{onclick}#{disabled}>
+      <i class="fas fa-save"></i> Сохранить
+      </button>
     )
     out.html_safe
   end
